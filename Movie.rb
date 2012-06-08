fd9b594db89275d1c7b61813b7d7a212e82488eb
sammbeller <sammbeller@gmail.com>
2012-06-08 13:30:15 -0400

    Movie.rb complete and commented

--- Movie.rb
+++ Movie.rb
@@ -1,21 +1,54 @@
 #Movie.rb 
 
-class Movie
 
+=begin
+
+	The Movie class represents an individual movie item.
+
+	@title : contains title of movie
+	@tags : contains an 18 element array of 1s and 0s representing the various
+	genres of the movie
+	@reviews : contains an array of all ratings of the movie
+
+
+=end
+
+
+class Movie
 
 	attr_reader :tags, :title, :reviews
 
 
+
+=begin
+
+	Array => Movie
+
+	takes an array representing the movie and initializes a Movie object
+
+	called with .new
+
+=end
+
 	def initialize (movie_array)
 
-		@title
+		@title movie_array[1]
 
-		@tags
+		@tags = movie_array[5..(movie_array - 1)]
 
 		@reviews = Array.new
 
 	end
 
+
+
+=begin
+
+	Review => Array
+
+	adds the rating field of a Review object to the Movie's array of ratings
+
+=end
 
 	def load_review(review)
 
@@ -24,11 +57,20 @@ class Movie
 	end
 
 
+
+=begin
+
+	=> int
+
+	returns the average rating of the Movie by summing the @reviews field and dividing it by its length
+
+=end
+
 	def average
 
 		@reviews.reduce(:+)/(@reviews.length) #sum reviews and divide by views
 
 	end
-	
+
 
 end
